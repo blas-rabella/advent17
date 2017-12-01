9d17157b5884e62d662aa31a6edfa90aba931e43
Blas Rabella <blas.rabella@hotmail.com>
2017-12-01 12:50:25 +0100

    Refactor for simplicity

--- day1/day1.hs
+++ day1/day1.hs
@@ -4,14 +4,11 @@ main :: IO ()
 main = do
   input <- getLine
   let nums = cycle $ map digitToInt input
-  print . sum . take (length input)$ zipWith f nums (getReady2 (length input) nums)
-
+  let n = length input
+  let offset = div n 2
+  -- drop 1 for sol 1
+  print . sum . take n $ zipWith f nums (drop offset nums)
 
 f :: (Num t, Eq t) => t -> t -> t
 f a b = if a == b then a else 0
 
-getReady2 :: Int -> [Int] -> [Int]
-getReady2 n = drop (div n 2)
-
-getReady :: [Int] -> [Int]
-getReady = tail
--- day1/day1.rs
+++ day1/day1.rs
@@ -7,9 +7,10 @@ fn main() {
         input.trim().chars().map(|c| c.to_digit(10).unwrap()).collect();
     let n = numbers.len();
     let mut result = 0;
+    // let offset = 1;
+    let offset = n/2;
     for i in 0..n {
-        // let j = (i+1) % n;
-        let j = (i+(n/2)) % n;
+        let j = (i + offset) % n;
         result = result + f(numbers[i], numbers[j]);
     }
     println!("result: {}", result)
